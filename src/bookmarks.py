68e5823ee1c6c8277b04ba26520585c5bad110bd
Gaurav Pandey <gauravthethinker@gmail.com>
2023-09-20 10:32:56 -0500

    new sample api

---
+++ src/bookmarks.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+bookmarks = Blueprint("bookmarks",__name__,url_prefix="/api/v1/bookmarks")
+
+@bookmarks.route('/', methods=['GET'])
+def get_all():
+    return {"bookmarks":[]}
+
